5a7d99c0166f1b7ee43ea0e20fdc67f328436374
HungPV <none@none.none>
2018-03-30 14:09:20 +0700

    Remove prj core-extended due to discard EthereumJ

--- 1.mvn-eclipse.cmd
+++ 1.mvn-eclipse.cmd
@@ -1,4 +1,3 @@
 start /D KeepMyCoin-core /W /B mvn eclipse:eclipse
-start /D KeepMyCoin-core-extended /W /B mvn eclipse:eclipse
 start /D KeepMyCoin /W /B mvn eclipse:eclipse
 start /D KeepMyCoin-Tools /W /B mvn eclipse:eclipse
--- 2.add-dependency.cmd
+++ 2.add-dependency.cmd
@@ -1,3 +1,1 @@
-java -jar export/AddPrjDependency.jar "KeepMyCoin-core" "KeepMyCoin-core-extended"
 java -jar export/AddPrjDependency.jar "KeepMyCoin-core" "KeepMyCoin"
-java -jar export/AddPrjDependency.jar "KeepMyCoin-core-extended" "KeepMyCoin"
